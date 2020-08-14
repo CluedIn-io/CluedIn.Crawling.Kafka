aeb9ca5ba49396fea4ca8b83769d02bfaf71786e
Ziggy <zygis0408@gmail.com>
2020-08-14 14:56:04 +0200

    added model

---
+++ src/Kafka.Core/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CluedIn.Crawling.Kafka.Core.Models
+{
+    public class Contact
+    {
+        public string AccountId { get; set; }
+        public string AccountIdName { get; set; }
+        public string AccountIdYomiName { get; set; }
+        public string AccountRoleCode { get; set; }
+        public string AccountRoleCodeName { get; set; }
+        public string ContactId { get; set; }
+        public string Description { get; set; }
+        public string FullName { get; set; }
+        public string NickName { get; set; }
+
+    }
+}
