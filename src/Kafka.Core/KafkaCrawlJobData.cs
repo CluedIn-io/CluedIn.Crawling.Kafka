1fad18feb157f4765d81ee588f44aa9fcabc9763
Ziggy <zygis0408@gmail.com>
2020-08-14 14:53:52 +0200

    fixed crawljobdata

--- src/Kafka.Core/KafkaCrawlJobData.cs
+++ src/Kafka.Core/KafkaCrawlJobData.cs
@@ -5,5 +5,9 @@ namespace CluedIn.Crawling.Kafka.Core
     public class KafkaCrawlJobData : CrawlJobData
     {
         public string ApiKey { get; set; }
+        public string KafkaServer { get; set; }
+        public string KafkaConnectionString { get; set; }
+        public string KafkaTopic { get; set; }
+        public int KafkaDummyClueGenerationInterval { get; set; }
     }
 }
